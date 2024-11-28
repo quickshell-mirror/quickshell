59298f6507106ab3488ce253650b7f02df89992a
outfoxxed <outfoxxed@outfoxxed.me>
2024-11-27 23:47:28 -0800

    i3: fix Q_PROPERTY definitions for use with typegen
    The regex currently can't handle line breaks

--- src/x11/i3/ipc/monitor.hpp
+++ src/x11/i3/ipc/monitor.hpp
@@ -9,41 +9,32 @@ namespace qs::i3::ipc {
 ///! I3/Sway monitors
 class I3Monitor: public QObject {
 	Q_OBJECT;
-
+	// clang-format off
 	/// The ID of this monitor
 	Q_PROPERTY(qint32 id READ id NOTIFY idChanged);
 	/// The name of this monitor
 	Q_PROPERTY(QString name READ name NOTIFY nameChanged);
 	/// Wether this monitor is turned on or not
 	Q_PROPERTY(bool power READ power NOTIFY powerChanged);
-
 	/// The current workspace
-	Q_PROPERTY(qs::i3::ipc::I3Workspace* focusedWorkspace READ focusedWorkspace NOTIFY
-	               focusedWorkspaceChanged);
-
+	Q_PROPERTY(qs::i3::ipc::I3Workspace* focusedWorkspace READ focusedWorkspace NOTIFY focusedWorkspaceChanged);
 	/// The X coordinate of this monitor inside the monitor layout
 	Q_PROPERTY(qint32 x READ x NOTIFY xChanged);
-
 	/// The Y coordinate of this monitor inside the monitor layout
 	Q_PROPERTY(qint32 y READ y NOTIFY yChanged);
-
 	/// The width in pixels of this monitor
 	Q_PROPERTY(qint32 width READ width NOTIFY widthChanged);
-
 	/// The height in pixels of this monitor
 	Q_PROPERTY(qint32 height READ height NOTIFY heightChanged);
-
 	/// The scaling factor of this monitor, 1 means it runs at native resolution
 	Q_PROPERTY(qreal scale READ scale NOTIFY scaleChanged);
-
 	/// Whether this monitor is currently in focus
 	Q_PROPERTY(bool focused READ focused NOTIFY focusedChanged);
-
 	/// Last JSON returned for this monitor, as a JavaScript object.
 	///
 	/// This updates every time Quickshell receives an `output` event from i3/Sway
 	Q_PROPERTY(QVariantMap lastIpcObject READ lastIpcObject NOTIFY lastIpcObjectChanged);
-
+	// clang-format on
 	QML_ELEMENT;
 	QML_UNCREATABLE("I3Monitors must be retrieved from the I3Ipc object.");
 
