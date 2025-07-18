e55d519c280192d8d97695b6c5905a0d7a46f8fe
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-07-18 14:43:09 +0400

    build: split derivation for extensible wrapper

--- default.nix
+++ default.nix
@@ -43,64 +43,93 @@
   withPam ? true,
   withHyprland ? true,
   withI3 ? true,
-}: buildStdenv.mkDerivation {
-  pname = "quickshell${lib.optionalString debug "-debug"}";
-  version = "0.1.0";
-  src = nix-gitignore.gitignoreSource [] ./.;
+}: let
+  unwrapped = buildStdenv.mkDerivation {
+    pname = "quickshell${lib.optionalString debug "-debug"}";
+    version = "0.1.0";
+    src = nix-gitignore.gitignoreSource "/default.nix\n" ./.;
 
-  nativeBuildInputs = [
-    cmake
-    ninja
-    qt6.qtshadertools
-    spirv-tools
-    qt6.wrapQtAppsHook
-    pkg-config
-  ]
-  ++ lib.optional withWayland wayland-scanner;
+    dontWrapQtApps = true; # see wrappers
 
-  buildInputs = [
-    qt6.qtbase
-    qt6.qtdeclarative
-    cli11
-  ]
-  ++ lib.optional withQtSvg qt6.qtsvg
-  ++ lib.optional withCrashReporter breakpad
-  ++ lib.optional withJemalloc jemalloc
-  ++ lib.optionals withWayland [ qt6.qtwayland wayland wayland-protocols ]
-  ++ lib.optionals (withWayland && libgbm != null) [ libdrm libgbm ]
-  ++ lib.optional withX11 xorg.libxcb
-  ++ lib.optional withPam pam
-  ++ lib.optional withPipewire pipewire;
+    nativeBuildInputs = [
+      cmake
+      ninja
+      qt6.qtshadertools
+      spirv-tools
+      pkg-config
+    ]
+    ++ lib.optional withWayland wayland-scanner;
 
-  cmakeBuildType = if debug then "Debug" else "RelWithDebInfo";
+    buildInputs = [
+      qt6.qtbase
+      qt6.qtdeclarative
+      cli11
+    ]
+    ++ lib.optional withQtSvg qt6.qtsvg
+    ++ lib.optional withCrashReporter breakpad
+    ++ lib.optional withJemalloc jemalloc
+    ++ lib.optionals withWayland [ qt6.qtwayland wayland wayland-protocols ]
+    ++ lib.optionals (withWayland && libgbm != null) [ libdrm libgbm ]
+    ++ lib.optional withX11 xorg.libxcb
+    ++ lib.optional withPam pam
+    ++ lib.optional withPipewire pipewire;
 
-  cmakeFlags = [
-    (lib.cmakeFeature "DISTRIBUTOR" "Official-Nix-Flake")
-    (lib.cmakeFeature "INSTALL_QML_PREFIX" qt6.qtbase.qtQmlPrefix)
-    (lib.cmakeBool "DISTRIBUTOR_DEBUGINFO_AVAILABLE" true)
-    (lib.cmakeFeature "GIT_REVISION" gitRev)
-    (lib.cmakeBool "CRASH_REPORTER" withCrashReporter)
-    (lib.cmakeBool "USE_JEMALLOC" withJemalloc)
-    (lib.cmakeBool "WAYLAND" withWayland)
-    (lib.cmakeBool "SCREENCOPY" (libgbm != null))
-    (lib.cmakeBool "SERVICE_PIPEWIRE" withPipewire)
-    (lib.cmakeBool "SERVICE_PAM" withPam)
-    (lib.cmakeBool "HYPRLAND" withHyprland)
-    (lib.cmakeBool "I3" withI3)
-  ];
+    cmakeBuildType = if debug then "Debug" else "RelWithDebInfo";
 
-  # How to get debuginfo in gdb from a release build:
-  # 1. build `quickshell.debug`
-  # 2. set NIX_DEBUG_INFO_DIRS="<quickshell.debug store path>/lib/debug"
-  # 3. launch gdb / coredumpctl and debuginfo will work
-  separateDebugInfo = !debug;
-  dontStrip = debug;
+    cmakeFlags = [
+      (lib.cmakeFeature "DISTRIBUTOR" "Official-Nix-Flake")
+      (lib.cmakeFeature "INSTALL_QML_PREFIX" qt6.qtbase.qtQmlPrefix)
+      (lib.cmakeBool "DISTRIBUTOR_DEBUGINFO_AVAILABLE" true)
+      (lib.cmakeFeature "GIT_REVISION" gitRev)
+      (lib.cmakeBool "CRASH_REPORTER" withCrashReporter)
+      (lib.cmakeBool "USE_JEMALLOC" withJemalloc)
+      (lib.cmakeBool "WAYLAND" withWayland)
+      (lib.cmakeBool "SCREENCOPY" (libgbm != null))
+      (lib.cmakeBool "SERVICE_PIPEWIRE" withPipewire)
+      (lib.cmakeBool "SERVICE_PAM" withPam)
+      (lib.cmakeBool "HYPRLAND" withHyprland)
+      (lib.cmakeBool "I3" withI3)
+    ];
 
-  meta = with lib; {
-    homepage = "https://quickshell.outfoxxed.me";
-    description = "Flexbile QtQuick based desktop shell toolkit";
-    license = licenses.lgpl3Only;
-    platforms = platforms.linux;
-    mainProgram = "quickshell";
+    # How to get debuginfo in gdb from a release build:
+    # 1. build `quickshell.debug`
+    # 2. set NIX_DEBUG_INFO_DIRS="<quickshell.debug store path>/lib/debug"
+    # 3. launch gdb / coredumpctl and debuginfo will work
+    separateDebugInfo = !debug;
+    dontStrip = debug;
+
+    meta = with lib; {
+      homepage = "https://quickshell.org";
+      description = "Flexbile QtQuick based desktop shell toolkit";
+      license = licenses.lgpl3Only;
+      platforms = platforms.linux;
+      mainProgram = "quickshell";
+    };
   };
-}
+
+  wrapper = unwrapped.stdenv.mkDerivation {
+    inherit (unwrapped) version meta buildInputs;
+    pname = "${unwrapped.pname}-wrapped";
+
+    nativeBuildInputs = unwrapped.nativeBuildInputs ++ [ qt6.wrapQtAppsHook ];
+
+    dontUnpack = true;
+    dontConfigure = true;
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      # cp will create .quickshell-wrapped in path, ln will not. It is occasionally useful.
+      cp -r ${unwrapped}/bin/* $out/bin
+      ln -s ${unwrapped}/share $out/share
+      # not /lib
+    '';
+
+    passthru = {
+      unwrapped = unwrapped;
+      withModules = modules: wrapper.overrideAttrs (prev: {
+        buildInputs = prev.buildInputs ++ modules;
+      });
+    };
+  };
+in wrapper
