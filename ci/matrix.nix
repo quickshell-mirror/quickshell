3804ffb47366cb7e571a9174b3f278195d0bfc7e
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-10 12:47:15 +0400

    fix ci by appropriately overriding the unwrapped drv instead of the wrapper

--- ci/matrix.nix
+++ ci/matrix.nix
@@ -4,5 +4,7 @@
 }: let
   nixpkgs = (import ./nix-checkouts.nix).${builtins.replaceStrings ["."] ["_"] qtver};
   compilerOverride = (nixpkgs.callPackage ./variations.nix {}).${compiler};
-  pkg = (nixpkgs.callPackage ../default.nix {}).override compilerOverride;
+  pkg = nixpkgs.callPackage ../default.nix {
+    quickshell-unwrapped = (nixpkgs.callPackage ../unwrapped.nix {}).override compilerOverride;
+  };
 in pkg
