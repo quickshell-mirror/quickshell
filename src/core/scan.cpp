quickshell-mirror/quickshell

commit 3e2ce40b18af943f9ba370ed73565e9f487663ef

core: reference configs by absolute instead of canonical paths

src/core/scan.cpp
@@ -163,7 +163,7 @@ bool QmlScanner::scanQmlFile(const QString& path, bool& singleton, bool& interna
 		qCDebug(logQmlScanner) << "Found imports" << imports;
 	}
 
-	auto currentdir = QDir(QFileInfo(path).canonicalPath());
+	auto currentdir = QDir(QFileInfo(path).absolutePath());
 
 	// the root can never be a singleton so it dosent matter if we skip it
 	this->scanDir(currentdir.path());
@@ -179,9 +179,9 @@ bool QmlScanner::scanQmlFile(const QString& path, bool& singleton, bool& interna
 		}
 
 		auto pathInfo = QFileInfo(ipath);
-		auto cpath = pathInfo.canonicalFilePath();
+		auto cpath = pathInfo.absoluteFilePath();
 
-		if (cpath.isEmpty()) {
+		if (!pathInfo.exists()) {
 			qCWarning(logQmlScanner) << "Ignoring unresolvable import" << ipath << "from" << path;
 			continue;
 		}

src/core/scan.hpp
@@ -16,9 +16,7 @@ public:
 	QmlScanner() = default;
 	QmlScanner(const QDir& rootPath): rootPath(rootPath) {}
 
-	// path must be canonical
 	void scanDir(const QString& path);
-
 	void scanQmlRoot(const QString& path);
 
 	QVector<QString> scannedDirs;

src/launch/command.cpp
@@ -110,7 +110,7 @@ int locateConfigFile(CommandState& cmd, QString& path) {
 					}
 
 					if (split[0].trimmed() == *cmd.config.name) {
-						path = QDir(QFileInfo(file).canonicalPath()).filePath(split[1].trimmed());
+						path = QDir(QFileInfo(file).absolutePath()).filePath(split[1].trimmed());
 						break;
 					}
 				}
@@ -140,8 +140,7 @@ int locateConfigFile(CommandState& cmd, QString& path) {
 				return -1;
 			}
 
-			path = QFileInfo(path).canonicalFilePath();
-			return 0;
+			goto rpath;
 		}
 	}
 
@@ -154,7 +153,8 @@ int locateConfigFile(CommandState& cmd, QString& path) {
 		return -1;
 	}
 
-	path = QFileInfo(path).canonicalFilePath();
+rpath:
+	path = QFileInfo(path).absoluteFilePath();
 	return 0;
 }
 

commit fc704e6b5d445899a1565955268c91942a4f263f

core: reference scanned paths by QDir over QString
Fixes a bug introduced in 3e2ce40 where a directory imported with a
"../name" path import would be passed to scanDir as ending in '/' which
created an invalid duplicate scan entry.

src/core/scan.cpp
@@ -19,12 +19,13 @@
 
 QS_LOGGING_CATEGORY(logQmlScanner, "quickshell.qmlscanner", QtWarningMsg);
 
-void QmlScanner::scanDir(const QString& path) {
-	if (this->scannedDirs.contains(path)) return;
-	this->scannedDirs.push_back(path);
+void QmlScanner::scanDir(const QDir& dir) {
+	if (this->scannedDirs.contains(dir)) return;
+	this->scannedDirs.push_back(dir);
+
+	const auto& path = dir.path();
 
 	qCDebug(logQmlScanner) << "Scanning directory" << path;
-	auto dir = QDir(path);
 
 	struct Entry {
 		QString name;
@@ -166,7 +167,7 @@ bool QmlScanner::scanQmlFile(const QString& path, bool& singleton, bool& interna
 	auto currentdir = QDir(QFileInfo(path).absolutePath());
 
 	// the root can never be a singleton so it dosent matter if we skip it
-	this->scanDir(currentdir.path());
+	this->scanDir(currentdir);
 
 	for (auto& import: imports) {
 		QString ipath;

src/core/scan.hpp
@@ -16,10 +16,10 @@ public:
 	QmlScanner() = default;
 	QmlScanner(const QDir& rootPath): rootPath(rootPath) {}
 
-	void scanDir(const QString& path);
+	void scanDir(const QDir& dir);
 	void scanQmlRoot(const QString& path);
 
-	QVector<QString> scannedDirs;
+	QVector<QDir> scannedDirs;
 	QVector<QString> scannedFiles;
 	QHash<QString, QString> fileIntercepts;
 