0a36e3ed40bf2de41b76ac363bc1f98820473786
outfoxxed <outfoxxed@outfoxxed.me>
2025-11-15 02:31:58 -0800

    ci: add qt6.10.0 checkout

--- ci/nix-checkouts.nix
+++ ci/nix-checkouts.nix
@@ -8,7 +8,12 @@ let
     inherit sha256;
   }) {};
 in rec {
-  latest = qt6_9_0;
+  latest = qt6_10_0;
+
+  qt6_10_0 = byCommit {
+    commit = "c5ae371f1a6a7fd27823bc500d9390b38c05fa55";
+    sha256 = "18g0f8cb9m8mxnz9cf48sks0hib79b282iajl2nysyszph993yp0";
+  };
 
   qt6_9_2 = byCommit {
     commit = "e9f00bd893984bc8ce46c895c3bf7cac95331127";
