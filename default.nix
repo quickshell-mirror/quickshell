d8f84c02540a04879c5f51138254d2e77a019953
Moraxyc <i@qaq.li>
2026-02-09 22:03:45 +0800

    nix: use libxcb directly

--- default.nix
+++ default.nix
@@ -18,7 +18,7 @@
   wayland,
   wayland-protocols,
   wayland-scanner,
-  xorg,
+  libxcb,
   libdrm,
   libgbm ? null,
   vulkan-headers,
@@ -88,7 +88,7 @@
     ++ lib.optional (withWayland && lib.strings.compareVersions qt6.qtbase.version "6.10.0" == -1) qt6.qtwayland
     ++ lib.optionals withWayland [ wayland wayland-protocols ]
     ++ lib.optionals (withWayland && libgbm != null) [ libdrm libgbm vulkan-headers ]
-    ++ lib.optional withX11 xorg.libxcb
+    ++ lib.optional withX11 libxcb
     ++ lib.optional withPam pam
     ++ lib.optional withPipewire pipewire
     ++ lib.optionals withPolkit [ polkit glib ];
